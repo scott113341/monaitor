b481b41debee00096d75063bb0a0101ae19b1bd8
Scott Hardy <scott.the.hardy@gmail.com>
2026-05-23 10:45:29 -0600

    Use my "ruby_llm-cost" gem again because ruby_llm isn't working

--- app/check.rb
+++ app/check.rb
@@ -55,7 +55,7 @@ class Check
     pp(response)
 
     outcome = response.content["determination"]
-    [outcome, screenshot, response, chat.cost.total]
+    [outcome, screenshot, response, chat.total_cost]
   end
 
   def self.run!(monitor)
